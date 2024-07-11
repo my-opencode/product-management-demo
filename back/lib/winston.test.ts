fb57ceb422ed72c8ceedbfbb55297c85caf0d8e5
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-11 22:40:04 +0200

    Fixes Init logger #14

--- back/lib/winston.test.ts
+++ back/lib/winston.test.ts
@@ -4,15 +4,20 @@ import assert from "node:assert";
 import path from "node:path";
 import Logger from "./winston";
 
+async function sleep(){
+  await new Promise (r => setTimeout(r,200));
+}
+
 describe(`Test log file creation`, function(){
   after(async function(){
       await fs.rm(path.resolve(__dirname,`../logs/error.log`));
       await fs.rm(path.resolve(__dirname,`../logs/tests.log`));
+      await sleep();
   });
   const logger = Logger(`tests`);
   it(`Should create a file ../logs/tests.log`, async function(){
     logger.info(`Create a tests log file.`);
-    await new Promise (r => setTimeout(r,200));
+    await sleep();
     const fnames = await fs.readdir(path.resolve(__dirname,`../logs`));
     assert(fnames.includes(`tests.log`));
   });
--- back/server.test.ts
+++ back/server.test.ts
@@ -4,14 +4,19 @@ import assert from "node:assert";
 import path from "node:path";
 import server from "./server";
 
+async function sleep(){
+  await new Promise (r => setTimeout(r,200));
+}
+
 describe(`Test log file creation`, function(){
   after(async function(){
       await fs.rm(path.resolve(__dirname,`./logs/error.log`));
       await fs.rm(path.resolve(__dirname,`./logs/server.log`));
+      await sleep();
   });
   it(`Should log errors to two files`, async function(){
     await server();
-    await new Promise (r => setTimeout(r,200));
+    await sleep();
     const errMsg = `This is an error log.`;
     const testsContents = await fs.readFile(path.resolve(__dirname,`./logs/server.log`), {encoding:`utf-8`});
     assert.strictEqual(

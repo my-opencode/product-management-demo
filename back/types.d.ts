58fdb816a6f62a4e95dc9ae96431f86f93ed98d1
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-16 17:24:21 +0200

    updated types with symbols

--- back/types.d.ts
+++ back/types.d.ts
@@ -1,5 +1,6 @@
 import { Express } from "express";
 import { Pool } from "mysql2/promise";
+import AppSymbols from "./AppSymbols";
 export interface RichApp extends Express {
-  get(value:"poolConnection"):Pool
+  get(value:keyof AppSymbols):Pool
 }

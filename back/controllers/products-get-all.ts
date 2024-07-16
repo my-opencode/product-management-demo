842d2f60820fa8ac5800beffe0048f63cdc72fa7
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-16 17:25:22 +0200

    added logs to controller

--- back/controllers/products-get-all.ts
+++ back/controllers/products-get-all.ts
@@ -1,13 +1,17 @@
 import { Response, Request, NextFunction, Express } from "express";
 import Product from "../models/Products";
 import objectToJSON from "../views/objectToJSON";
+import Logger from "../lib/winston";
+const logger = Logger(`controllers/products-get-all`);
 
 export default async function productsGetAll(request: Request, response: Response, next: NextFunction) {
+  logger.log(`debug`,`Entering productsGetAll`);
   try {
     const productList = await Product.listFromDatabase(request.app as Express);
     const payload = objectToJSON(productList);
-    response.status(200).json(payload);
+    logger.log(`debug`,`Exiting productsGetAll`);
   } catch(err){
+    logger.log(`debug`,`Error in productsGetAll`);
     next(err);
   }
 }

220ee302935c89c7d50b8db8412d64a5f2a4f223
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-16 17:29:30 +0200

    init server with express Fixes Init server #16

--- back/server.test.ts
+++ back/server.test.ts
@@ -3,6 +3,9 @@ import fs from "node:fs/promises";
 import assert from "node:assert";
 import path from "node:path";
 import startServer from "./server";
+import inject, { Response } from "light-my-request";
+import { Express } from "express";
+import { ProductAsInTheJson } from "./models/Products";
 
 async function sleep() {
   await new Promise(r => setTimeout(r, 200));
@@ -18,11 +21,16 @@ describe(`Test log file creation`, function () {
     await sleep();
   });
   it(`Should log notices to file`, async function () {
-    await startServer();
+    await startServer({ skipListen: true, skipDatabase: true, skipRoutes: true });
     await sleep();
     // const errMsg = `This is an error log.`;
     const testsContents = await fs.readFile(path.resolve(__dirname, `./logs/server.log`), { encoding: `utf-8` });
-    const firstLog = JSON.parse(testsContents.split(`\n`).filter(v => !!v).slice(-1)[0]);
+    const firstLog = JSON.parse(
+      testsContents
+        .split(`\n`)
+        .filter(v => !!v)
+        .find(l => l.includes(`{"level":"info","message":"Initializing start server sequence.","service":"server"}`)) || ``
+    );
     assert.strictEqual(
       firstLog.message,
       `Initializing start server sequence.`
@@ -33,6 +41,42 @@ describe(`Test log file creation`, function () {
     //   JSON.parse(errorContents.split(`\n`).filter(v=>!!v).slice(-1)[0]).message,
     //   errMsg
     // );
+  });
+});
+
+describe(`Test API endpoints`, function () {
+  let app: Express;
+  before(async function () {
+    app = await startServer({ skipListen: true });
+  });
+  after(function(){
+    app.emit(`close`);
+  });
+  describe(`404`, function () {
+    it(`should return status code 404`, async function () {
+      const response = await inject(app, { method: `get`, url: `/` });
+      assert.strictEqual(response.statusCode, 404);
+      assert.strictEqual(response.payload, `Resource "/" not found.`);
+    });
+  });
+  describe(`GET /products`, async function () {
+    let response: Response;
+    let json: Array<ProductAsInTheJson>;
+    await before(async function () {
+      response = await inject(app, { method: `get`, url: `/products` });
+    });
+    it(`should return status code 200`, function () {
+      assert.strictEqual(response.statusCode, 200);
+    });
+    it(`should return json array`, function () {
+      assert.strictEqual(typeof response.payload, `string`);
+      json = JSON.parse(response.payload);
+      console.log(typeof json, json.length);
+      assert.strictEqual(Array.isArray(json), true);
+    });
+    it(`should return all products`, function () {
+      assert.strictEqual(json.length, 30);
+    });
   });
 });
 
--- back/server.ts
+++ back/server.ts
@@ -1,21 +1,67 @@
 
 import Logger from "./lib/winston";
+import connector, { waitForDbServer } from "./database/connector";
+import express from "express";
+import setRoutes from "./routes/router";
+import { Server } from "http";
+import AppSymbols from "./AppSymbols";
 const logger = Logger(`server`);
+const PORT = process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000;
+export interface StartServerOptions {
+  skipDatabase?: boolean;
+  skipRoutes?: boolean;
+  skipListen?: boolean;
+}
 /**
  * Entry point for the server.
  */
-export default async function startServer(){
-  const logger = Logger(`server`);
+export default async function startServer(options?: StartServerOptions) {
+  const app = express();
 
-  logger.log(`info`,`Initializing start server sequence.`);
-  logger.log(`debug`,`Connecting DB.`);
-  await waitForDbServer();
-  const connectionPool = connector();
-  logger.log(`debug`, `Connected DB.`);
-  
-  logger.log(`debug`, `Disconnecting DB.`);
-  connectionPool.end();
-  logger.log(`debug`, `Disonnected DB.`);
+  let disconnectDatabase = function () { };
+  logger.log(`info`, `Initializing start server sequence.`);
+
+  if (!options?.skipDatabase) {
+    logger.log(`debug`, `Connecting DB.`);
+    await waitForDbServer();
+    const connectionPool = connector();
+    logger.log(`debug`, `Connected DB.`);
+    disconnectDatabase = function () {
+      logger.log(`debug`, `Disconnecting DB.`);
+      connectionPool.end();
+      logger.log(`debug`, `Disonnected DB.`);
+    }
+    app.set(AppSymbols.connectionPool, connectionPool);
+  } else logger.log(`warn`, `Skipping Database`);
+
+  if (!options?.skipRoutes)
+    setRoutes(app);
+  else logger.log(`warn`, `Skipping Routes`);
+
+  let server: Server;
+  if (!options?.skipListen)
+    server = app.listen(PORT, () => { logger.log(`info`, `Server listening on ${PORT}.`); });
+  else logger.log(`warn`, `Skipping Listening`);
+
+  const shutdown = async function () {
+    if (!options?.skipListen) {
+      await new Promise(r => server.close(r));
+      logger.log(`info`, `Server closed.`);
+    }
+    disconnectDatabase();
+  }
+  app.on(`close`, shutdown);
+  process.on('SIGTERM', () => {
+    logger.log(`warning`, `Received SIGTERM signal: closing server.`);
+    shutdown();
+  });
+  process.on('SIGINT', () => {
+    logger.log(`warning`, `Received SIGINT signal: closing server.`);
+    shutdown();
+  });
+
+  await new Promise(r => process.nextTick(r));
+  return app;
 }
 
 if (require.main === module) {

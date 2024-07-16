3f8e05e89ddafa7a06c117a2871d84ba2de24fa8
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-16 18:16:38 +0200

    error handler added to router

--- back/routes/router.ts
+++ back/routes/router.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import productsRouter from "./products";
 import default404 from "../controllers/default.404";
+import errorHandler from "../controllers/errorHandler";
 
 export default function setRoutes(app: Express) {
   app.use(`/products`, productsRouter);
+  app.use(errorHandler);
   app.use(default404);
 }

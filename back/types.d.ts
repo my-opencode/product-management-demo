f8bd63b764110ca367c9cd81067ab0d96f1703c2
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-17 18:21:59 +0200

    updated types

--- back/types.d.ts
+++ back/types.d.ts
@@ -1,6 +1,18 @@
-import { Express } from "express";
+import { Application, Express, Request } from "express";
 import { Pool } from "mysql2/promise";
 import AppSymbols from "./AppSymbols";
-export interface RichApp extends Express {
-  get(value:keyof AppSymbols):Pool
+import Product from "./models/Products";
+import * as core from "express-serve-static-core";
+
+export interface RichApp extends Express.Application, Record<string, any> {
+  get(value: keyof AppSymbols): Pool
+}
+export interface RequestWithId extends Request<core.ParamsDictionary, any, any, core.Query, Record<string, any>> {
+  app: RichApp;
+  id?: number;
+}
+export interface RequestWithProduct extends RequestWithId, Request<core.ParamsDictionary, any, any, core.Query, Record<string, any>> {
+  app: RichApp;
+  id?: number;
+  product?: Product
 }

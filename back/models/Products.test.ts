fbd9b7b4d92ca323f880be73ec69929de33cfc47
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-18 22:53:22 +0200

    product constructor

--- back/models/Products.test.ts
+++ back/models/Products.test.ts
@@ -2,6 +2,7 @@ import { before, beforeEach, describe, it, mock } from "node:test";
 import * as assert from "assert";
 import Product from "./Products";
 import AppSymbols from "../AppSymbols";
+import { ValidationError } from "../lib/validators";
 
 describe(`Product class`, function () {
   let app: any;
@@ -92,6 +93,77 @@ describe(`Product class`, function () {
         result,
         { id: 1 }
       );
+    });
+  });
+  describe(`new Product`, function(){
+    describe(`invalid id`, function(){
+      it(`should throw`, function(){
+        assert.throws(
+          ()=>new Product({ id: (`invalid` as unknown as number)}),
+          new TypeError(`Invalid 'id' value.`)
+        );
+      });
+    });
+    describe(`invalid values`, function(){
+      it(`should throw`, function(){
+        assert.throws(
+          ()=>new Product({
+            image: ``.padEnd(2049,"."),
+            rating: 6
+          }),
+          new ValidationError(`Invalid Product: ${
+            [
+              `code: Too short. Min length: 1.`,
+              `name: Too short. Min length: 1.`,
+              `description: Too short. Min length: 1.`,
+              `image: Too long. Max length: 2048.`,
+              `category: Invalid foreign id key.`,
+              `quantity: Too low. Min value: 0.`,
+              `price: Too low. Min value: 0.01.`,
+              `rating: Too high. Max value: 5.`,
+            ].join(`; `)
+          }`)
+        );
+      });
+    });
+    describe(`unsaved`, function(){
+      let p:Product;
+      it(`should validate`, function(){
+        p = new Product({
+          code: `abc`,
+          name: `product abc`,
+          description: `product desc`,
+          image: `abc.png`,
+          category: 1,
+          quantity: 10,
+          price: 100,
+          rating: 3
+        });
+        assert.ok(true);
+      });
+      it(`should have isSaved false`, function(){
+        assert.strictEqual(p.isSaved, false);
+      });
+    });
+    describe(`saved`, function(){
+      let p:Product;
+      it(`should validate`, function(){
+        p = new Product({
+          id: 12,
+          code: `abc`,
+          name: `product abc`,
+          description: `product desc`,
+          image: `abc.png`,
+          category: 1,
+          quantity: 10,
+          price: 100,
+          rating: 3
+        });
+        assert.ok(true);
+      });
+      it(`should have isSaved false`, function(){
+        assert.strictEqual(p.isSaved, true);
+      });
     });
   });
 });
--- back/models/Products.ts
+++ back/models/Products.ts
@@ -1,8 +1,13 @@
 import { RowDataPacket } from "mysql2";
 import { RichApp } from "../types";
 import AppSymbols from "../AppSymbols";
+import Id from "./Id";
+import { validateFloat, validateInt, validateString, validateTinyInt, ValidationError } from "../lib/validators";
 
-export interface ProductAsInTheJson extends RowDataPacket {
+export interface ProductAsInTheJson extends RowDataPacket, ProductBase {
+}
+
+export interface ProductBase {
   id: number;
   code: string;
   name: string;
@@ -12,14 +17,133 @@ export interface ProductAsInTheJson extends RowDataPacket {
   category: number;
   quantity: number;
   inventoryStatus: string;
-  rating: number;
+  rating?: number;
 }
 
 export class Product {
-  static async list(app:RichApp){
+  isSaved = false;
+  _id: number | undefined = undefined;
+  _code = ``;
+  _name = ``;
+  _description = ``;
+  _image: string | undefined = ``;
+  _price = -1;
+  _category = -1;
+  _quantity = -1;
+  _inventoryStatus = ``;
+  _rating = -1;
+  get id(): number | undefined {
+    return this._id;
+  }
+  set id(val: number | undefined) {
+    if (val === undefined) {
+      this._id = undefined;
+      this.isSaved = false;
+    } else {
+      console.log(`this is the id value to test "${val}"`)
+      this._id = Id.validator(val);
+      if (this._id)
+        this.isSaved = true;
+    }
+  }
+  get code(): string {
+    return this._code;
+  }
+  set code(val: string) {
+    this._code = validateString(val, 255, 1);
+  }
+  get name(): string {
+    return this._name;
+  }
+  set name(val: string) {
+    this._name = validateString(val, 1024, 1);
+  }
+  get description(): string {
+    return this._description;
+  }
+  set description(val: string) {
+    this._description = validateString(val, 5120, 1);
+  }
+  get image(): string | undefined {
+    return this._image;
+  }
+  set image(val: string | undefined) {
+    if (val === undefined)
+      this._image = undefined;
+    else
+      this._image = validateString(val, 2048) || undefined;
+  }
+  get category(): number {
+    return this._category;
+  }
+  set category(val: number) {
+    try {
+      this._category = Id.validator(val);
+    } catch (err){
+      throw new ValidationError(`Invalid foreign id key.`);
+    }
+  }
+  get quantity(): number {
+    return this._quantity;
+  }
+  set quantity(val: number) {
+    this._quantity = validateInt(val, 16777215, 0);
+  }
+  get price(): number {
+    return this._price;
+  }
+  set price(val: number | string) {
+    this._price = validateFloat(val, undefined, 0.01);
+  }
+  get rating(): number {
+    return this._rating;
+  }
+  set rating(val: number | undefined) {
+    if (val === undefined)
+      this._rating = 0;
+    else
+      this._rating = validateTinyInt(val, 5, 0);
+  }
+  get inventoryStatus() {
+    return this._inventoryStatus;
+  }
+  set inventoryStatus(val: string) {
+    const inventoryStatusValues = [
+      `OUTOFSTOCK`,
+      `LOWSTOCK`,
+      `INSTOCK`
+    ];
+    if (val && inventoryStatusValues.includes(val.toUpperCase()))
+      this._inventoryStatus = val.toUpperCase();
+  }
+  constructor(val: Partial<ProductBase>) {
+    // invalid id is breaking
+    this.id = val.id;
+    // aggregate validation errors
+    const validationErrors: string[] = [];
+    const valErrHandler = (field: string, e: any) => {
+      if (e instanceof ValidationError)
+        validationErrors.push(field + `: ` + e.message);
+      else throw e;
+    };
+    // validate & set fields
+    try { this.code = val.code || ``; } catch (e) { valErrHandler(`code`, e); }
+    try { this.name = val.name || ``; } catch (e) { valErrHandler(`name`, e); }
+    try { this.description = val.description || ``; } catch (e) { valErrHandler(`description`, e); }
+    try { this.image = val.image; } catch (e) { valErrHandler(`image`, e); }
+    try { this.category = val.category || -1; } catch (e) { valErrHandler(`category`, e); }
+    try { this.quantity = val.quantity || -1; } catch (e) { valErrHandler(`quantity`, e); }
+    try { this.price = val.price || -1; } catch (e) { valErrHandler(`price`, e); }
+    try { this.rating = val.rating; } catch (e) { valErrHandler(`rating`, e); }
+    // throw for invalid fields
+    if (validationErrors.length) {
+      throw new ValidationError(`Invalid Product: ${validationErrors.join(`; `)}`);
+    }
+  }
+  static async list(app: RichApp) {
     return await this.listFromDatabase(app);
   }
-  static async listFromDatabase(app:RichApp){
+  static async listFromDatabase(app: RichApp) {
     const pool = app.get(AppSymbols.connectionPool);
     const [rows] = await pool.execute(`SELECT 
       p.id, 
@@ -52,10 +176,10 @@ RIGHT JOIN (
 WHERE deleted = 0;`);
     return rows as ProductAsInTheJson[];
   }
-  static async getById(app:RichApp, id:number){
+  static async getById(app: RichApp, id: number) {
     return await this.getFromDatabaseById(app, id);
   }
-  static async getFromDatabaseById(app:RichApp, id:number):Promise<ProductAsInTheJson|undefined>{
+  static async getFromDatabaseById(app: RichApp, id: number): Promise<ProductAsInTheJson | undefined> {
     const pool = app.get(AppSymbols.connectionPool);
     const [rows] = await pool.execute(`SELECT 
       p.id, 

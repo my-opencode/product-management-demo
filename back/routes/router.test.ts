fe3d7a214c65802766351f67ab71a1acbd861923
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-22 19:33:45 +0200

    added validation error handler to router

--- back/routes/router.test.ts
+++ back/routes/router.test.ts
@@ -9,11 +9,6 @@ import { RequestHandler, Router } from "express";
 import { describe, it } from "node:test";
 import * as assert from "node:assert";
 import router from "./router";
-import categoriesGetAll from "../controllers/categories-get-all";
-import productsGetAll from "../controllers/products-get-all";
-import productsGetOneById from "../controllers/products-get-one-by-id";
-import errorHandler from "../controllers/errorHandler";
-import default404 from "../controllers/default.404";
 
 
 describe(`Router`, function () {
@@ -24,7 +19,7 @@ describe(`Router`, function () {
     assert.ok(Array.isArray(router.stack));
   });
   it(`of layers`, function(){
-    assert.strictEqual(router.stack.length, 4);
+    assert.strictEqual(router.stack.length, 5);
   });
   it(`categories layer`, function(){
     assert.ok(router.stack[0].regexp.toString().includes(`categories`));
@@ -38,63 +33,13 @@ describe(`Router`, function () {
     //@ts-ignore
     assert.strictEqual((router.stack[1].handle as Router).params.id.length, 2);
   });
+  it(`validationErrorHandler layer`, function(){
+    assert.strictEqual(router.stack[2].name, `validationErrorHandler`);
+  });
   it(`errorHandler layer`, function(){
-    assert.strictEqual(router.stack[2].name, `errorHandler`);
+    assert.strictEqual(router.stack[3].name, `errorHandler`);
   });
   it(`default404 layer`, function(){
-    assert.strictEqual(router.stack[3].name, `default404`);
+    assert.strictEqual(router.stack[4].name, `default404`);
   });
-
-  // const stack: [string, ["get" | "post" | "patch" | "delete" | "put" | "all", RequestHandler][]][] = [
-  //   [`/:id`, [
-  //     [`get`, productsGetOneById],
-  //   ]],
-  //   [`/`, [
-  //     [`get`, productsGetAll],
-  //   ]]
-  // ];
-  // it(`that is not empty`, function () {
-  //   assert.strictEqual(router.stack.length, stack.length);
-  // });
-  // stack.forEach(([url, substack], i) => {
-  //   it(`with ${url}`, function () {
-  //     assert.strictEqual(router.stack[i]?.route?.path, url);
-  //   });
-  //   substack.forEach(([method, controller]) => {
-  //     it(`with ${method} ${url} controller`, function () {
-  //       assert.strictEqual(router.stack[i]?.route?.stack?.[0]?.method, method);
-  //       assert.strictEqual(router.stack[i]?.route?.stack?.[0]?.handle, controller);
-  //     });
-  //   });
-  // });
 });
-
-// describe(`Router routes setter`, function () {
-//   let app: any;
-//   before(function () {
-//     app = {
-//       use: mock.fn((...handlers: RequestHandler[]) => { })
-//     };
-//     assert.strictEqual(app.use.mock.callCount(), 0);
-//     setRoutes(app as unknown as Express);
-//   });
-//   it(`should call app.use for each sub route`, function () {
-//     assert.strictEqual(app.use.mock.callCount(), 4);
-//   });
-//   it(`should add products routes`, function () {
-//     const call = app.use.mock.calls[0];
-//     assert.strictEqual(call.arguments?.[0], `/categories`);
-//   });
-//   it(`should add products routes`, function () {
-//     const call = app.use.mock.calls[1];
-//     assert.strictEqual(call.arguments?.[0], `/products`);
-//   });
-//   it(`should add use error route`, function () {
-//     const call = app.use.mock.calls[2];
-//     assert.deepStrictEqual(call.arguments, [errorHandler]);
-//   });
-//   it(`should add use 404 route`, function () {
-//     const call = app.use.mock.calls[3];
-//     assert.deepStrictEqual(call.arguments, [default404]);
-//   });
-// });
--- back/routes/router.ts
+++ back/routes/router.ts
@@ -3,10 +3,12 @@ import productsRouter from "./products";
 import default404 from "../controllers/default.404";
 import errorHandler from "../controllers/errorHandler";
 import categoriesRouter from "./categories";
+import validationErrorHandler from "../controllers/validation-error-handler";
 
 const router = express.Router();
 router.use(`/categories`, categoriesRouter);
 router.use(`/products`, productsRouter);
+router.use(validationErrorHandler);
 router.use(errorHandler);
 router.use(default404);
 export default router;

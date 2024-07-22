c4969f4d328f493fad19476a2abe56cce16f4659
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-22 21:14:07 +0200

    update validators

--- back/lib/validators.test.ts
+++ back/lib/validators.test.ts
@@ -1,134 +1,186 @@
 import assert from "node:assert";
 import { describe, it } from "node:test";
-import { validateFloat, validateInt, validateMediumInt, validateSmallInt, validateString, validateTinyInt, ValidationError } from "./validators";
+import { validateFloat, validateInt, validateMediumInt, validateSmallInt, validateString, validateTinyInt, ValidationError, ValidationErrorStack } from "./validators";
 
-describe(`validateString`, function(){
-  it(`should throw too long`, function(){
+describe(`ValidationError class`, function () {
+  it(`should allow 1 argument`, function () {
+    const e = new ValidationError(`error message`);
+    assert.strictEqual(e.message, `error message`);
+  });
+  it(`should allow 2 arguments`, function () {
+    const e = new ValidationError(`error message`, `name of the field`);
+    assert.strictEqual(e.fieldName, `name of the field`);
+  });
+  it(`should allow 3 arguments`, function () {
+    const e = new ValidationError(`error message`, `name of the field`, 409);
+    assert.strictEqual(e.statusCode, 409);
+  });
+  it(`should convert to formatted string`, function () {
+    const e = String(new ValidationError(`error message`, `name of the field`, 409));
+    assert.strictEqual(e, `ValidationError: name of the field: error message`);
+  });
+});
+describe(`ValidationErrorStack class`, function () {
+  it(`should only accept arrays of ValidationErrors as first argument (1)`, function () {
     assert.throws(
-      ()=>validateString(`   `,2),
-      new ValidationError(`Too long. Max length: 2.`)
+      //@ts-ignore
+      () => new ValidationErrorStack(`not array`)
+      , TypeError
     );
   });
-  it(`should throw too short`, function(){
+  it(`should only accept arrays of ValidationErrors as first argument (2)`, function () {
     assert.throws(
-      ()=>validateString(`  `,undefined,4),
-      new ValidationError(`Too short. Min length: 4.`)
+      //@ts-ignore
+      () => new ValidationErrorStack([`not error`])
+      , TypeError
     );
   });
-  it(`should return a string`, function(){
+  it(`should only accept arrays of ValidationErrors as first argument (3)`, function () {
+    assert.throws(
+      //@ts-ignore
+      () => new ValidationErrorStack([new Error(`something`)])
+      , TypeError
+    );
+  });
+  it(`should allow 2 arguments`, function () {
+    const e = new ValidationErrorStack([new ValidationError(`error message`)],`stack message`);
+    assert.strictEqual(e.message, `stack message`);
+    assert.strictEqual(e[0].message, `error message`);
+  });
+  it(`should allow 3 arguments`, function () {
+    const e = new ValidationErrorStack([new ValidationError(`error message`)],`stack message`, 444);
+    assert.strictEqual(e.statusCode, 444);
+  });
+  it(`should convert to formatted string`, function () {
+    const e = String(new ValidationErrorStack([new ValidationError(`error message`)],`stack message`, 444));
+    assert.strictEqual(e, `ValidationErrorStack: stack message; Stack: ValidationError: unknown field: error message`);
+  });
+});
+
+describe(`validateString`, function () {
+  it(`should throw too long`, function () {
+    assert.throws(
+      () => validateString(`   `, 2),
+      new ValidationError(`Too long. Max length: 2.`, `string`)
+    );
+  });
+  it(`should throw too short`, function () {
+    assert.throws(
+      () => validateString(`  `, undefined, 4),
+      new ValidationError(`Too short. Min length: 4.`, `string`)
+    );
+  });
+  it(`should return a string`, function () {
     const result = validateString(`hello`);
     assert.strictEqual(result, `hello`);
   });
-  it(`should accept empty string`, function(){
+  it(`should accept empty string`, function () {
     const result = validateString(``);
     assert.strictEqual(result, ``);
   });
 });
 
-describe(`validateFloat`, function(){
-  it(`should throw NaN`, function(){
+describe(`validateFloat`, function () {
+  it(`should throw NaN`, function () {
     assert.throws(
-      ()=>validateFloat(`not a number`),
-      new ValidationError(`Not an finite float.`)
+      () => validateFloat(`not a number`),
+      new ValidationError(`Not an finite float.`, `float`)
     );
   });
-  it(`should throw infinite`, function(){
+  it(`should throw infinite`, function () {
     assert.throws(
-      ()=>validateFloat(Number.POSITIVE_INFINITY),
-      new ValidationError(`Not an finite float.`)
+      () => validateFloat(Number.POSITIVE_INFINITY),
+      new ValidationError(`Not an finite float.`, `float`)
     );
   });
-  it(`should throw too high`, function(){
+  it(`should throw too high`, function () {
     assert.throws(
-      ()=>validateFloat(100000.1),
-      new ValidationError(`Too high. Max value: 99999.99.`)
+      () => validateFloat(100000.1),
+      new ValidationError(`Too high. Max value: 99999.99.`, `float`)
     );
   });
-  it(`should throw too low`, function(){
+  it(`should throw too low`, function () {
     assert.throws(
-      ()=>validateFloat(2,undefined,2.1),
-      new ValidationError(`Too low. Min value: 2.1.`)
+      () => validateFloat(2, undefined, 2.1),
+      new ValidationError(`Too low. Min value: 2.1.`, `float`)
     );
   });
-  it(`should return a float`, function(){
+  it(`should return a float`, function () {
     const result = validateFloat(`1.5`);
     assert.strictEqual(result, 1.5);
   });
-  it(`should accept 0`, function(){
+  it(`should accept 0`, function () {
     const result = validateFloat(0);
     assert.strictEqual(result, 0);
   });
-  it(`should accept negative`, function(){
+  it(`should accept negative`, function () {
     const result = validateFloat(`-0.5`);
     assert.strictEqual(result, -0.5);
   });
 });
 
-describe(`validateInt`, function(){
-  it(`should throw NaN`, function(){
+describe(`validateInt`, function () {
+  it(`should throw NaN`, function () {
     assert.throws(
-      ()=>validateInt(`not a number`),
-      new ValidationError(`Not an finite integer.`)
+      () => validateInt(`not a number`),
+      new ValidationError(`Not an finite integer.`, `int`)
     );
   });
-  it(`should throw infinite`, function(){
+  it(`should throw infinite`, function () {
     assert.throws(
-      ()=>validateInt(Number.POSITIVE_INFINITY),
-      new ValidationError(`Not an finite integer.`)
+      () => validateInt(Number.POSITIVE_INFINITY),
+      new ValidationError(`Not an finite integer.`, `int`)
     );
   });
-  it(`should throw too high`, function(){
+  it(`should throw too high`, function () {
     assert.throws(
-      ()=>validateInt(4294967296),
-      new ValidationError(`Too high. Max value: 4294967295.`)
+      () => validateInt(4294967296),
+      new ValidationError(`Too high. Max value: 4294967295.`, `int`)
     );
   });
-  it(`should throw too low`, function(){
+  it(`should throw too low`, function () {
     assert.throws(
-      ()=>validateInt(2,undefined,3),
-      new ValidationError(`Too low. Min value: 3.`)
+      () => validateInt(2, undefined, 3),
+      new ValidationError(`Too low. Min value: 3.`, `int`)
     );
   });
-  it(`should return an integer`, function(){
+  it(`should return an integer`, function () {
     const result = validateInt(`1.5`);
     assert.strictEqual(result, 1);
   });
-  it(`should accept 0`, function(){
+  it(`should accept 0`, function () {
     const result = validateInt(0);
     assert.strictEqual(result, 0);
   });
-  it(`should accept negative`, function(){
+  it(`should accept negative`, function () {
     const result = validateInt(`-5`);
     assert.strictEqual(result, -5);
   });
 });
-<<<<<<< HEAD
-=======
 
-describe(`validateMediumInt`, function(){
-  it(`should throw too high`, function(){
+describe(`validateMediumInt`, function () {
+  it(`should throw too high`, function () {
     assert.throws(
-      ()=>validateMediumInt(16777216),
-      new ValidationError(`Too high. Max value: 16777215.`)
+      () => validateMediumInt(16777216),
+      new ValidationError(`Too high. Max value: 16777215.`, `int`)
     );
   });
 });
 
-describe(`validateMediumInt`, function(){
-  it(`should throw too high`, function(){
+describe(`validateMediumInt`, function () {
+  it(`should throw too high`, function () {
     assert.throws(
-      ()=>validateSmallInt(65536),
-      new ValidationError(`Too high. Max value: 65535.`)
+      () => validateSmallInt(65536),
+      new ValidationError(`Too high. Max value: 65535.`, `int`)
     );
   });
 });
 
-describe(`validateMediumInt`, function(){
-  it(`should throw too high`, function(){
+describe(`validateTinyInt`, function () {
+  it(`should throw too high`, function () {
     assert.throws(
-      ()=>validateTinyInt(256),
-      new ValidationError(`Too high. Max value: 255.`)
+      () => validateTinyInt(256),
+      new ValidationError(`Too high. Max value: 255.`, `int`)
     );
   });
 });
->>>>>>> 94f0b04 (generic validators)
--- back/lib/validators.ts
+++ back/lib/validators.ts
@@ -1,38 +1,80 @@
-export class ValidationError extends Error { };
-export function validateString(val: unknown, maxLength = 65535, minLength = 0) {
+export class ValidationErrorStack extends Array {
+  message = ``;
+  statusCode = 422;
+  /**
+   * ValidationErrorStack constructor. Takes an array of validation errors and an optional description
+   * @param {ValidationError[]} errors stack of validation errors
+   * @param {string} [message] description of the stack of validation errors. Example: 'invalid product'
+   */
+  constructor(errors: ValidationError[], message?: string, statusCodeOverride?:number) {
+    if(!Array.isArray(errors) || !errors.every(e => e instanceof ValidationError)) throw new TypeError(`Expecting array of ValidationErrors`);
+    super();
+    this.push(...errors);
+    if (message) this.message = message;
+    if(statusCodeOverride && statusCodeOverride > 99 && statusCodeOverride < 600)
+      this.statusCode = statusCodeOverride;
+  }
+  toString(){
+    let str = `ValidationErrorStack: ` + this.message + `; Stack: `;
+    // fixes bug with VES.map(e=>String(e)).join() that calls constructor with the string value of errors.
+    const errs = [...this];
+    if(this.length) str += errs.map(e=>String(e)).join(`, `);
+    else str += `empty`;
+    return str;
+  }
+}
+export class ValidationError extends Error {
+  fieldName: string = "unknown field";
+  statusCode = 422;
+  constructor(msg: string, fieldName?: string, statusCodeOverride?:number) {
+    super(msg);
+    if (fieldName)
+      this.fieldName = fieldName;
+    if(statusCodeOverride && statusCodeOverride > 99 && statusCodeOverride < 600)
+      this.statusCode = statusCodeOverride;
+  }
+  toString(){
+    return `ValidationError: ` + this.fieldName + `: ` + this.message;
+  }
+};
+export function validateString(val: unknown, maxLength = 65535, minLength = 0, fieldName?: string) {
+  fieldName = fieldName || `string`;
   let _val = String(val);
   if (maxLength && _val.length > maxLength)
-    throw new ValidationError(`Too long. Max length: ${maxLength}.`);
+    throw new ValidationError(`Too long. Max length: ${maxLength}.`, fieldName);
   if (minLength && _val.length < minLength)
-    throw new ValidationError(`Too short. Min length: ${minLength}.`);
+    throw new ValidationError(`Too short. Min length: ${minLength}.`, fieldName);
   return _val;
 }
-export function validateFloat(val: unknown, maxValue = 99999.99, minValue = -99999.99) {
+export function validateFloat(val: unknown, maxValue = 99999.99, minValue = -99999.99, fieldName?: string) {
+  fieldName = fieldName || `float`;
   let _val = parseFloat(String(val));
   if (isNaN(_val) || !isFinite(_val))
-    throw new ValidationError(`Not an finite float.`);
+    throw new ValidationError(`Not an finite float.`, fieldName);
   if (maxValue && _val > maxValue)
-    throw new ValidationError(`Too high. Max value: ${maxValue}.`);
+    throw new ValidationError(`Too high. Max value: ${maxValue}.`, fieldName);
   if (minValue !== undefined && _val < minValue)
-    throw new ValidationError(`Too low. Min value: ${minValue}.`);
+    throw new ValidationError(`Too low. Min value: ${minValue}.`, fieldName);
   return _val;
 }
-export function validateInt(val: unknown, maxValue = 4294967295, minValue = -2147483648) {
+export function validateInt(val: unknown, maxValue = 4294967295, minValue = -2147483648, fieldName?: string) {
+  fieldName = fieldName || `int`;
   let _val = parseInt(String(val));
   if (isNaN(_val) || !isFinite(_val))
-    throw new ValidationError(`Not an finite integer.`);
+    throw new ValidationError(`Not an finite integer.`, fieldName);
   if (maxValue && _val > maxValue)
-    throw new ValidationError(`Too high. Max value: ${maxValue}.`);
+    throw new ValidationError(`Too high. Max value: ${maxValue}.`, fieldName);
   if (minValue !== undefined && _val < minValue)
-    throw new ValidationError(`Too low. Min value: ${minValue}.`);
+    throw new ValidationError(`Too low. Min value: ${minValue}.`, fieldName);
   return _val;
 }
-export function validateMediumInt(val: unknown, maxValue = 16777215, minValue = -8388608) {
-  return validateInt(val, maxValue, minValue);
+
+export function validateMediumInt(val: unknown, maxValue = 16777215, minValue = -8388608, fieldName?: string) {
+  return validateInt(val, maxValue, minValue, fieldName);
 }
-export function validateSmallInt(val: unknown, maxValue = 65535, minValue = -32768) {
-  return validateInt(val, maxValue, minValue);
+export function validateSmallInt(val: unknown, maxValue = 65535, minValue = -32768, fieldName?: string) {
+  return validateInt(val, maxValue, minValue, fieldName);
 }
-export function validateTinyInt(val: unknown, maxValue = 255, minValue = -128) {
-  return validateInt(val, maxValue, minValue);
+export function validateTinyInt(val: unknown, maxValue = 255, minValue = -128, fieldName?: string) {
+  return validateInt(val, maxValue, minValue, fieldName);
 }

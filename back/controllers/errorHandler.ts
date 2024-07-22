633760bfa2c4017b77cb6e2ff189f47fe458eafd
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-22 19:31:36 +0200

    fixed empty error in error handler

--- back/controllers/errorHandler.ts
+++ back/controllers/errorHandler.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import Logger from "../lib/winston";
-const logger = Logger(`ErrorHandler`);
+const logger = Logger(`controllers/ErrorHandler`);
 export default function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+  if(!err) return next();
   if (res.headersSent) {
     return next(err);
   }
-  if(!err) return next();
-  logger.log(`error`, `Caught error for ${req.method} "${req.url}" — ${err.message}`);
+  logger.log(`error`, `Caught error for ${req.method} "${req.url}" — ${err.message} — Trace: ${err.stack}`);
   res.status(500).send(`Unexpected error. Please contact our support if the error persists.`);
 }

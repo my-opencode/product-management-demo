f6e8550e5119f53110141a55e751b0e8f663c09a
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-24 09:39:20 +0200

    updated routes & tests

--- back/routes/products.ts
+++ back/routes/products.ts
@@ -5,12 +5,14 @@ import productsParamMwGetProductById from "../middleware/products-param-id";
 import paramValidatorMwId from "../middleware/param-id";
 import productsCreate from "../controllers/products-post-create";
 import jsonBodyParser from "../lib/jsonBodyParser";
+import productsUpdateOneByID from "../controllers/products-patch-update-one-by-id";
 
 const productsRouter = express.Router();
 productsRouter.param(`id`, paramValidatorMwId);
 productsRouter.param(`id`, productsParamMwGetProductById);
 
 productsRouter.get(`/:id`, productsGetOneById);
+productsRouter.patch(`/:id`, jsonBodyParser, productsUpdateOneByID);
 productsRouter.get(`/`, productsGetAll);
 productsRouter.post(`/`, jsonBodyParser, productsCreate);
 

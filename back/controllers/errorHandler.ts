d79d3631633f6730273126b66af28a532fd6f166
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-27 10:05:32 +0200

    user facing message from a variable

--- back/controllers/errorHandler.ts
+++ back/controllers/errorHandler.ts
@@ -6,10 +6,11 @@ export default function errorHandler(err: Error, req: Request, res: Response, ne
   if (res.headersSent) {
     return next(err);
   }
-  logger.log(`error`, `Caught error for ${req.method} "${req.url}" — ${err.message} — Trace: ${err.stack}`);
+  const message = `Unexpected error. Please contact our support if the error persists.`;
+  logger.log(`error`, `Caught error for ${req.method} "${req.url}" — \n${err.message} — \nTrace: ${err.stack}`);
   // todo move to view
   if(req.headers?.accept === `application/json`)
-    res.status(500).set(`Accept`,`application/json`).send(`{"description":"Unexpected error. Please contact our support if the error persists.",errors:[]}`);
+    res.status(500).set(`Accept`,`application/json`).send(`{"description":"${message}",errors:[]}`);
   else
-    res.status(500).send(`Unexpected error. Please contact our support if the error persists.`);
+    res.status(500).send(message);
 }

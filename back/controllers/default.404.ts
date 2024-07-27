0087aff8bd31914c9dda04663a63c9af137b02f5
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-27 10:05:56 +0200

    support json response

--- back/controllers/default.404.ts
+++ back/controllers/default.404.ts
@@ -2,6 +2,13 @@ import { Request, Response } from "express";
 import Logger from "../lib/winston";
 const logger = Logger(`controllers/404-handler`,`debug`);
 export default function default404 (req:Request,res:Response){
-  logger.log(`debug`, `Resource "${req.url}" not found.`)
-  res.status(404).send(`Resource "${req.url}" not found.`);
+  const message = `Resource "${req.url}" not found.`;
+  logger.log(`debug`, message);
+  // todo move to view
+  if(req.headers?.accept === `application/json`)
+    res.status(404)
+    .set(`Accept`,`application/json`)
+    .send(`{"description":"${message}",errors:[{"${req.url}":"${message}"}]}`);
+  else
+    res.status(404).send(message);
 }

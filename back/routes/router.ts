28412f34ec9f2966a9bb1a275b7358c0b394fd22
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-28 16:48:10 +0200

    updated controller imports

--- back/routes/router.ts
+++ back/routes/router.ts
@@ -1,7 +1,7 @@
 import express, { Express } from "express";
 import productsRouter from "./products";
-import default404 from "../controllers/default.404";
-import errorHandler from "../controllers/errorHandler";
+import default404 from "../controllers/default-404";
+import errorHandler from "../controllers/error-handler";
 import categoriesRouter from "./categories";
 import validationErrorHandler from "../controllers/validation-error-handler";
 

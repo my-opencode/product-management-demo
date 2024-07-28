e935472318e26e16cacfa4f46f82892b95d839b9
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-28 16:37:31 +0200

    renamed category-list.test.ts

--- back/views/category-list.unit.test.ts
+++ back/views/category-list.unit.test.ts
@@ -1,7 +1,6 @@
 import assert from "node:assert";
 import { describe, it } from "node:test";
 import renderer from "./category-list";
-import { RowDataPacket } from "mysql2";
 
 describe(`category list renderer`, function () {
   it(`should return a json string`, function () {

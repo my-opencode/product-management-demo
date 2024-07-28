515a3b487e6a7bc465ee84b67aead82f2276b24e
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-28 16:49:14 +0200

    renamed products.test.ts

--- back/routes/products.unit.test.ts
+++ back/routes/products.unit.test.ts
@@ -4,10 +4,10 @@ import productsRouter from "./products";
 import productsGetAll from "../controllers/products-get-all";
 import productsGetOneById from "../controllers/products-get-one-by-id";
 import { RequestHandler } from "express";
-import productsCreate from "../controllers/products-post-create";
-import jsonBodyParser from "../lib/jsonBodyParser";
-import productsUpdateOneById from "../controllers/products-patch-update-one-by-id";
-import productsDeleteOneById from "../controllers/products-delete-delete-one-by-id";
+import productsCreate from "../controllers/products-create";
+import jsonBodyParser from "../lib/json-body-parser";
+import productsUpdateOneById from "../controllers/products-update-one-by-id";
+import productsDeleteOneById from "../controllers/products-delete-one-by-id";
 
 describe(`Products router`, function () {
   it(`should return a router function`, function () {

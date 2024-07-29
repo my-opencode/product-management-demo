815d1d4f67821622ce45949f78f289eee20322f5
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-29 11:32:00 +0200

    improved category types #64

--- back/models/category.ts
+++ back/models/category.ts
@@ -1,20 +1,22 @@
 import { RowDataPacket } from "mysql2";
 import { RichApp } from "../types";
 import AppSymbols from "../AppSymbols";
+import { DirectCategorySelectExecuteResponse } from "../database/adapter-response-format";
 
-export interface CategoryFromDb extends RowDataPacket {
+export interface CategoryBase {
   id: number;
   name: string;
 }
 
+export interface CategoryFromDb extends RowDataPacket, CategoryBase { }
 export class Category {
   static async list(app:RichApp){
     return await this.listFromDatabase(app);
   }
   static async listFromDatabase(app:RichApp){
     const pool = app.get(AppSymbols.connectionPool);
-    const [rows] = await pool.execute(`SELECT id, name FROM ProductCategories;`);
-    return rows as CategoryFromDb[];
+    const [rows] = await pool.execute<DirectCategorySelectExecuteResponse>(`SELECT id, name FROM ProductCategories;`);
+    return rows;
   }
 }
 

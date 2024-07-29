efe18b0124c1781641697e15131b9db614202815
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-29 11:40:06 +0200

    updated headerSent=true response

--- back/controllers/error-handler.ts
+++ back/controllers/error-handler.ts
@@ -4,12 +4,10 @@ import renderer from "../views/generic-error";
 const logger = Logger(`controllers/error-handler`);
 export default function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
   if(!err) return next();
-  if (res.headersSent) {
-    return next(err);
-  }
   const message = `Unexpected error. Please contact our support if the error persists.`;
   logger.log(`error`, `Caught error for ${req.method} "${req.url}" — \n${err.message} — \nTrace: ${err.stack}`);
-
+  if (res.headersSent) {
+    return res.send(message);
+  }
   res.status(500).send(renderer(message));
-}
 }

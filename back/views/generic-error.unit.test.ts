86a0ff5add7d513e9a33f26377a4e49ccb8921bc
LA <71209534+my-opencode@users.noreply.github.com>
2024-07-29 09:47:53 +0200

    Added generic error view

---
+++ back/views/generic-error.unit.test.ts
@@ -0,0 +1,16 @@
+import assert from "node:assert";
+import { describe, it } from "node:test";
+import renderer from "./generic-error";
+
+describe(`Generic error renderer`, function () {
+  it(`should return json string`, function () {
+    assert.strictEqual(
+      typeof renderer(new Error(`Oops`)), 
+      `string`
+    );
+    assert.strictEqual(
+      renderer(new Error(`Oops`)), 
+      `{"description":"Oops"}`
+    );
+  });
+});
